e040334cbb1e9db8054f735f3070c6b52b09924a
naufaladikbds <naufal.adi@kbdsi.com>
2022-07-20 08:42:37 +0700

    First Commit
    First commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  print('WEEEE');
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Text('bruh'),
+      ),
+    );
+  }
+}
